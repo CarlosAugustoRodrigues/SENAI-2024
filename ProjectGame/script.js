3bd3d4f65e7a221e752fd7213724fe0c69e218ea
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-01-23 19:11:24 -0300

    SENAI

--- ProjectGame/script.js
+++ ProjectGame/script.js
@@ -1,19 +1,62 @@
-var time = 60;
+var time = 10;
+var timeStart = 3;
 var score = 0;
-const timer = document.querySelector('#timer')
+var record;
+const timer = document.querySelector('#timer');
 const displayScore = document.querySelector('#score');
-const divScore = document.querySelector('.div-score')
 const gameBoard = document.querySelectorAll('.div-container');
-const container = document.querySelector('.container')
-const startText = document.querySelector('.start-text')
+const startText = document.querySelector('.start-text');
+const containerGame = document.querySelector('.container-game');
+const timeStartGame = document.querySelector('.time-start-game');
 
-displayScore.innerHTML = score;
-timer.innerHTML = time;
+
+function game() {
+    displayScore.innerHTML = score;
+    timer.innerHTML = time;
+    
+    gameBoard.forEach((e) => {
+        e.addEventListener('click', () => {
+            if(e.classList.contains('active')) {
+                score += 7;
+                displayScore.innerHTML = score;
+            } if(!e.classList.contains('active')) {
+                score -= 3;
+                displayScore.innerHTML = score;
+            }
+            e.classList.remove('active')
+        })
+    })
+
+    setTimeout(() => {
+        setInterval(() => {
+            time--;
+            timer.innerHTML = time;
+        }, 1000)
+    }, 500)
+}
 
 startText.addEventListener('click', () => {
-    container.style.display = 'flex';
-    timer.style.display = 'block'
-    divScore.style.display = 'flex'
     startText.style.display = 'none';
+    timeStartGame.style.display = 'flex';
+
+    setTimeout(() => {
+        timeStartGame.innerHTML = timeStart;
+        setInterval(() => {
+            timeStart--
+            timeStartGame.innerHTML = timeStart;
+
+            if(timeStart == 0) {
+                timeStartGame.innerHTML = 'GO';
+            }
+        }, 1000);
+    }, 500);
+
+    setTimeout(() => {
+        timeStartGame.style.display = 'none'
+        containerGame.style.display = 'flex';
+        game();
+    }, 4000)
 })
+
+
 
