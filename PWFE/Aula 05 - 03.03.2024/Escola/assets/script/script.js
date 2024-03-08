f1df079b9bd2e90717c274e4d4d4e4c245070cf4
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-03-07 21:53:45 -0300

    SENAI

--- PWFE/Aula 05 - 03.03.2024/Escola/assets/script/script.js
+++ PWFE/Aula 05 - 03.03.2024/Escola/assets/script/script.js
@@ -1,15 +1,44 @@
 const modal =  document.querySelectorAll('dialog');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('main button');
 
 
 // Modal Contato
-function showModal() {
+function openModal() {
     body.style.overflowY = 'hidden'
     modal[0].showModal()
 }
-function removeModal() {
+function closeModal() {
     modal[0].close()
 }
-modal[0].addEventListener('close', () => {
-    body.style.overflowY = 'auto'
-})
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const btnId = button.getAttribute('data-button-id')
+
+        if(btnId === '1'){
+            body.style.overflow = 'hidden';
+            modal[1].showModal();
+        } else if (btnId === '2'){
+            body.style.overflow = 'hidden';
+            modal[2].showModal();
+        } else if(btnId === '3'){
+            body.style.overflow = 'hidden';
+            modal[3].showModal();
+        } else  if(btnId === '4'){
+            body.style.overflow = 'hidden';
+            modal[4].showModal();
+        } else {
+            body.style.overflow = 'hidden';
+            modal[5].showModal();
+        }
+    })
+});
+
+
+modal.forEach((e) => {
+    e.addEventListener('close', () => {
+        body.style.overflowY = 'auto'
+    });
+});
