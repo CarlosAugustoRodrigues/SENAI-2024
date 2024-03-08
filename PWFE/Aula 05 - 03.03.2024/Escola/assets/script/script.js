bf010824f479e4193f4f367dc72822620b6a0bfd
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-03-08 08:03:01 -0300

    SENAI

--- PWFE/Aula 05 - 03.03.2024/Escola/assets/script/script.js
+++ PWFE/Aula 05 - 03.03.2024/Escola/assets/script/script.js
@@ -1,15 +1,13 @@
 const modal =  document.querySelectorAll('dialog');
 const body = document.querySelector('body');
 const buttons = document.querySelectorAll('main button');
+const btnClose = document.querySelectorAll('.btn-close');
 
 
 // Modal Contato
 function openModal() {
     body.style.overflowY = 'hidden'
     modal[0].showModal()
-}
-function closeModal() {
-    modal[0].close()
 }
 
 
@@ -33,7 +31,28 @@ buttons.forEach((button) => {
             body.style.overflow = 'hidden';
             modal[5].showModal();
         }
-    })
+    });
+});
+
+btnClose.forEach((btnClose) => {
+    btnClose.addEventListener("click", () => {
+        const btnCloseId = btnClose.getAttribute('data-button-close-id')
+
+        if(btnCloseId === '0') {
+            modal[0].close()
+        } else if(btnCloseId === '1') {
+            modal[1].close()
+        } else if(btnCloseId === '2') {
+            modal[2].close()
+        } else if(btnCloseId === '3') {
+            modal[3].close()
+        } else if(btnCloseId === '4') {
+            modal[4].close()
+        } else {
+            modal[5].close()
+        }
+
+    });
 });
 
 
