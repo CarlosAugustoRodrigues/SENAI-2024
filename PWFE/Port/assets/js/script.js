c2aa0efd258d8210a227a4106eacb512cf5f35c0
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-05-05 22:33:25 -0300

    SENAI

--- PWFE/Port/assets/js/script.js
+++ PWFE/Port/assets/js/script.js
@@ -1,5 +1,9 @@
 const header = document.querySelector('header');
+let date = new Date()
+let currentYear = date.getFullYear();
 
 window.addEventListener('scroll', function() {
     header.classList.toggle('opacity', window.scrollY > 0);
 });
+
+document.querySelector('#year').textContent = currentYear;
