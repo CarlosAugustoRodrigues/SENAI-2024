b2c4f21111353a507d29f31e55cc8c9ec18a92a6
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-05-22 11:19:17 -0300

    SENAI

--- PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/destinos.js
+++ PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/destinos.js
@@ -1,11 +1,28 @@
 const uri = "http://localhost:3000/destinos";
 const itens = [];
 
-function loadItens(){
+function loadItens() {
     fetch(uri)
     .then(response => response.json())
     .then(data => {
         itens.push(...data);
+        renderItens();
         console.log(itens);
+    });
+};
+
+function renderItens() {
+    const main = document.querySelector('main');
+    const card = document.querySelector('#card');
+
+    itens.forEach( (item) => {
+        let cardNew = card.cloneNode(true);
+        cardNew.classList.remove('model');
+        cardNew.querySelector('#cidade').textContent = item.nome
+        cardNew.querySelector('#valor').textContent = `R$${item.valor}`
+        cardNew.querySelector('#data').textContent = item.data
+        cardNew.querySelector('#hoteis').textContent = item.pontos.length
+        cardNew.querySelector('#pontos').textContent = item.hoteis.length
+        main.appendChild(cardNew);
     });
 };
--- PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/hoteis.js
+++ PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/hoteis.js
@@ -9,3 +9,19 @@ function loadItens(){
         console.log(itens);
     });
 };
+
+function renderItens() {
+    const main = document.querySelector('main');
+    const card = document.querySelector('#card');
+
+    itens.forEach( (item) => {
+        let cardNew = card.cloneNode(true);
+        cardNew.classList.remove('model');
+        cardNew.querySelector('#cidade').textContent = item.nome
+        cardNew.querySelector('#valor').textContent = `R$${item.valor}`
+        cardNew.querySelector('#data').textContent = item.data
+        cardNew.querySelector('#hoteis').textContent = item.pontos.length
+        cardNew.querySelector('#pontos').textContent = item.hoteis.length
+        main.appendChild(cardNew);
+    });
+};
