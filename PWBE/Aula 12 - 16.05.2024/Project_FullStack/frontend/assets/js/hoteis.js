5260311c69ebe6146b585c9bbd33c9b8d203e271
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-05-29 11:22:33 -0300

    SENAI

--- PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/hoteis.js
+++ PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/hoteis.js
@@ -5,7 +5,7 @@ var itens = [];
 
 function loadItens(){
     itens = []
-    main.textContent = ''
+    // main.textContent = ''
     fetch(uri)
     .then(response => response.json())
     .then(data => {
@@ -20,28 +20,35 @@ function renderItens() {
         let cardNew = card.cloneNode(true);
         cardNew.classList.remove('model');
         cardNew.setAttribute('data-idH', item.id);
-        cardNew.querySelector('#hotel').textContent = item.nome
-        cardNew.querySelector('#valor').textContent = `R$${item.valor}`
-        cardNew.querySelector('#endereco').textContent = item.endereco
-        cardNew.querySelector('#email').textContent = item.email
-        cardNew.querySelector('#site').textContent = item.site
-        cardNew.querySelector('#telefone').textContent = item.telefones[0].telefone
+        cardNew.querySelector('#hotel').textContent = item.nome;
+        cardNew.querySelector('#valor').textContent = `R$${item.valor}`;
+        cardNew.querySelector('#endereco').textContent = item.endereco;
+        cardNew.querySelector('#email').textContent = item.email;
+        cardNew.querySelector('#site').textContent = item.site;
+
+        if(item.telefones.length == 0) {
+            cardNew.querySelector('#telefone').textContent = 'Não informado';
+            cardNew.querySelector('#telefone-2').textContent = 'Não informado';
+        } else {
+            cardNew.querySelector('#telefone').textContent = item.telefones[0].telefone;
+            item.telefones.length < 2 ? cardNew.querySelector('#telefone-2').textContent = 'Não informado' : cardNew.querySelector('#telefone-2').textContent = item.telefones[1].telefone;
+        }
 
         switch(item.avaliacao) {
             case 1:
-                cardNew.querySelector('.info-at img').setAttribute('src', '../img/1estrela.png')
+                cardNew.querySelector('.info-at img').setAttribute('src', '../img/1estrela.png');
                 break;
             case 2:
-                cardNew.querySelector('.info-at img').setAttribute('src', '../img/2estrelas.png')
+                cardNew.querySelector('.info-at img').setAttribute('src', '../img/2estrelas.png');
                 break;
             case 3:
-                cardNew.querySelector('.info-at img').setAttribute('src', '../img/3estrelas.png')
+                cardNew.querySelector('.info-at img').setAttribute('src', '../img/3estrelas.png');
                 break;
             case 4:
-                cardNew.querySelector('.info-at img').setAttribute('src', '../img/4estrelas.png')
+                cardNew.querySelector('.info-at img').setAttribute('src', '../img/4estrelas.png');
                 break;
             case 5:
-                cardNew.querySelector('.info-at img').setAttribute('src', '../img/5estrelas.png')
+                cardNew.querySelector('.info-at img').setAttribute('src', '../img/5estrelas.png');
                 break;
         }
 
