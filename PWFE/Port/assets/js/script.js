8e35804eaa121367a8bfba93207d74d485445d3d
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-06-04 10:13:57 -0300

    SENAI

--- PWFE/Port/assets/js/script.js
+++ PWFE/Port/assets/js/script.js
@@ -1,12 +1,7 @@
-const header = document.querySelector('header');
 const cursorDot = document.querySelector('[data-cursor-dot]');
 const cursorOutline = document.querySelector('[data-cursor-outline]');
 let date = new Date()
 let currentYear = date.getFullYear();
-
-window.addEventListener('scroll', function() {
-    header.classList.toggle('effect-blur', window.scrollY > 0);
-});
 
 document.querySelector('#year').textContent = currentYear;
 
