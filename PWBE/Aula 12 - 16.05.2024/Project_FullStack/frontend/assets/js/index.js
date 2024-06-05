9272aa506741f5dac579ccc508b5cadc929d2cad
CarlosAugustoRodrigues <carlos.augusto01503@hotmail.com>
2024-06-05 11:06:40 -0300

    SENAI

--- PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/index.js
+++ PWBE/Aula 12 - 16.05.2024/Project_FullStack/frontend/assets/js/index.js
@@ -1,11 +1,6 @@
 const data = new Date();
-const header = document.querySelector('header');
 
 function currentYear() {
     const year = data.getFullYear();
     this.document.querySelector('#data').textContent = year;
 };
-
-window.addEventListener('scroll', () => {
-    header.classList.toggle('effect-blur', window.scrollY > 0);
-});
